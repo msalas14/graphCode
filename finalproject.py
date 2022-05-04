1bb1e619d8b822b04e66a2313884bf72a3a0fc93
msalas14 <mjso1104@gmail.com>
2022-05-04 18:06:06 -0400

    comments

--- finalproject.py
+++ finalproject.py
@@ -6,6 +6,7 @@ from graph import LinkedDirectedGraph
 from random import *
 from graphics import *
 import dijkstra
+
 g = LinkedDirectedGraph()
 def G(g,N):
     ''' G(N) for any N>= 1'''
@@ -16,14 +17,15 @@ def G(g,N):
     blocked = []
     centerPts = []
    
-
-    if(N>=1):
-        while  y<desired:
-            while(x<desired):
+    
+    if(N>=1): 
+        '''Graphics set up and blocked cells chosen (using index)'''
+        while  y<desired: # make "columns"
+            while(x<desired): # make "rows"
                 sq = Rectangle(Point(x,y), Point(x+50,y+50))
-                centerPts.append(sq.getCenter())
+                centerPts.append(sq.getCenter()) #add Point object of coordinates of current cell's center
                 if (x == (desired-50) and y == (desired-50)) or (x==0 and y==0):
-                    '''start and end cells'''
+                    '''if start and end cells'''
                     sq.setFill("aquamarine3")
                     sq.draw(workArea)
                     message = Text(Point(x+25,y+25),messages[currText])
@@ -31,8 +33,8 @@ def G(g,N):
                     currText+=1
                     
                 elif random()<.25: 
-                    blocked.append(cellCt)
                     '''create blocked cell'''
+                    blocked.append(cellCt) #add index of cell to the bloacked array
                     #blocked cells will not have up/down/left/right
                     sq.setFill("gray26")
                     sq.draw(workArea)
@@ -48,59 +50,52 @@ def G(g,N):
         for i in range(N*N): #ADD VERTICES TO GRAPH
             g.addVertex(i)
         for i in range(N*N): #ADD ALL EDGES TO GRAPH
-            if i not in blocked:
+            if i not in blocked: #only add edges for cells that are not blocked
                 if(N<=i):   # has up?
-                    if(i-N) not in blocked:
+                    if(i-N) not in blocked: #only add edges for vertices that have an up vertex that isn't blocked
                         g.addEdge(i,i-N,1)
                         line = Line(centerPts[i],centerPts[i-N])
                         line.setFill("red")
                         line.draw(workArea)
-                    # else:
-                    #     g.addEdge(i, i-N, None)
                 if( i%N != 0): #has left?
-                    if(i-1) not in blocked:
+                    if(i-1) not in blocked: #only add edges for vertices that have a left vertex that isn't blocked
                         g.addEdge(i,i-1,1)
                         line = Line(centerPts[i],centerPts[i-1])
                         line.setFill("red")
                         line.draw(workArea)
-                    # else:
-                    #     g.addEdge(i, i-1, None)
                 if((i+1)%N!=0): #has right?
-                    if(i+1) not in blocked:
+                    if(i+1) not in blocked: #only add edges for vertices that have a right vertex that isn't blocked
                         g.addEdge(i, i+1,1)
                         line = Line(centerPts[i],centerPts[i+1])
                         line.setFill("red")
                         line.draw(workArea)
-                    # else:
-                    #     g.addEdge(i, i+1, None)
                 if(i<(N*(N-1))): #has down?
-                    if(i+N) not in blocked:
+                    if(i+N) not in blocked: #only add edges for vertices that have a down vertex that isn't blocked
                         g.addEdge(i,i+N,1)
                         line = Line(centerPts[i],centerPts[i+N])
                         line.setFill("red")
                         line.draw(workArea)
-                    # else:
-                    #     g.addEdge(i, i+N, None)
         
     else:
         print("invalid G(N)")
-    return centerPts
+    return centerPts #return array of Point objects for each cell's center point
     
 def puzzleGraph(workArea, N):
-    points = G(g,N)
-    print(g)
+    points = G(g,N); ''' make structured graph for g Graph and store the center points array from the graphics grid '''
+    #graph 
+    print(g) #print graph representation
 
-    '''input for testing dijkstra and confirming unsolvable graph'''
-    #val = input("Enter: ")
-    #print(val)
-    #if val == "yes":
+    V=dijkstra.dijkstraPuzzle(g,0)  #store dijkstra algorithm V array
+    #shortest path solved
 
-    V=dijkstra.dijkstraPuzzle(g,0)
+    currIndex = (N*N)-1 #start at last index
+    nextIndex=0 #init variable
+    pathStr = "->" + str(currIndex)
 
-    currIndex = (N*N)-1
-    nextIndex=0
-    while(currIndex>0):
-        nextIndex = V[currIndex]
+
+    ''' Representation for the shortest path '''
+    while(currIndex>0): #while the path being created hasn't reached the beginning vertex/cell
+        nextIndex = V[currIndex] 
         if(currIndex!=None and nextIndex!=None):
 
             line = Line(points[currIndex],points[nextIndex])
@@ -113,31 +108,15 @@ def puzzleGraph(workArea, N):
             text.setTextColor("Red")
             text.setSize(30)
             text.draw(workArea)
+            pathStr = "unsolvable "
             break
-        
-    #index is the vertex
-    #value of index is connecting vertex
-    '''Drawing the solution'''
-    # i = 0
-    # while i < len(V):
-    #     if V[i] != None:
-    #         line = Line(points[i],points[V[i]])
-    #         line.setFill("blue")
-    #         line.setWidth(6)
-    #         line.draw(workArea)
-    #         #print(V[i])
-    #     i+=1
-    '''steps to get the solution to draw'''
-    # use the last {index} or N^2 -1 to store point
-    # store point of the value of the last index (*store the value as the next index*)
-    # line for those two points
-    # use prev point
-    # * store point of the value of this index (*store the value as the next index*)
-    # until index is 0???
+        if(currIndex!= 0):
+            pathStr = "->" + str(currIndex) + pathStr
+        else:
+            pathStr = "0" + pathStr
+    print("Path: " , pathStr)
 
-
-
-    #print(cellCt)
+    ''' Graphics Window Exit Code'''
     while True:
         try:
             if workArea.getMouse().getX()!=None: #exit window by clicking on window with mouse
@@ -147,5 +126,5 @@ def puzzleGraph(workArea, N):
     workArea.close() #exits the window after any of the aforementioned actions occur
 
 
-workArea = GraphWin("Puzzle Graph", 600,600)
-puzzleGraph(workArea,9)
+workArea = GraphWin("Puzzle Graph", 600,600) #Make graphics window
+puzzleGraph(workArea,9) #construct puzzle graph on the graphics window for a 9x9 grid
